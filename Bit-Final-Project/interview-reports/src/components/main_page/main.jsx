6732d757e0a69d6887c6e89c43857ba9fd1133ad
Jovan Davidovic <davidovic.12@gmail.com>
2023-03-27 23:04:56 +0200

    some work on main page

--- Bit-Final-Project/interview-reports/src/components/main_page/main.jsx
+++ Bit-Final-Project/interview-reports/src/components/main_page/main.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+
 export const Main = () => {
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3333/api/candidates")
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  }, []);
+
+  console.log(user);
 
 
-    return <></>
-    
-}
+  const renderCandidates = () => {
+    return Object.values(user).map((value, index)=> {
+        return (
+            <div key={value.id}>
+                <img src={value.avatar} alt="" />
+                <p>{value.name}</p>
+            </div>
+        )
+    })
+  }
+
+  return <div>{renderCandidates()}</div>;
+};
