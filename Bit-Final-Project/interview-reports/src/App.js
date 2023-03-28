0c65e6c20280979353939675b35fc65c2dc5b928
AnesKostreba <aneskostreba@yahoo.com>
2023-03-28 23:06:34 +0200

    Styling-main

--- Bit-Final-Project/interview-reports/src/App.js
+++ Bit-Final-Project/interview-reports/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import { Main } from './components/main_page/main';
+import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
-    <div className="App">
-      <Main/>
       
-    </div>
+      <Main/>
   );
 }
 
