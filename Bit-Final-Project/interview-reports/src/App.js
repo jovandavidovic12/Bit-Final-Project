86e2e820531f0f1929369b63d05292c78740bf12
AnesKostreba <aneskostreba@yahoo.com>
2023-04-01 15:50:33 +0200

    Footer

--- Bit-Final-Project/interview-reports/src/App.js
+++ Bit-Final-Project/interview-reports/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CandidateReport from './components/candidate-reports/CandidateReport';
 import { Main } from './components/main_page/main';
 import Header from './components/header/Header';
+import { Footer } from './components/footer/Footer';
 
 function App() {
   return (
@@ -11,9 +12,11 @@ function App() {
       <BrowserRouter>
         <Header/>
         <Routes>
-            <Route path = '/home'index element={<Main />} />
+            <Route index element={<Main />} />
             <Route path='/candidate-reports' element={<CandidateReport />} />
+
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
