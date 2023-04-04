226b85a478e3576adb99aa9a0080a770ca3ba90a
Jovan Davidovic <davidovic.12@gmail.com>
2023-04-04 19:32:11 +0200

    updating

--- Bit-Final-Project/interview-reports/src/App.js
+++ Bit-Final-Project/interview-reports/src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CandidateReport from './components/candidate-reports/CandidateReport';
-import { Main } from './components/main_page/main';
+import { Main } from './components/main_page/Main-page';
 import { Error } from './components/page-error/Error';
 import { Footer } from './components/footer/Footer';
-
+import Header from './components/header/Header';
 function App() {
   return (
     <div className="App">
