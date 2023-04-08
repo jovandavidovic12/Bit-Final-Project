10ef9a49a8b562c5cb0baa939446a3487f259680
Jovan Davidovic <davidovic.12@gmail.com>
2023-04-08 12:17:15 +0200

    final update before doomsday

--- Bit-Final-Project/interview-reports/src/components/card/Card.jsx
+++ Bit-Final-Project/interview-reports/src/components/card/Card.jsx
@@ -1,33 +1,27 @@
 import "./card.css";
 import { Link } from "react-router-dom";
-export const Card = (user) => {
-  // console.log(user)
+export const Card = ({ data }) => {
+  console.log(data);
 
   return (
     <Link
-      to={`/candidate-reports/${user.data.id}`}
+      to={`/candidate-reports/${data.id}`}
       className="glavni d-flex justify-content-center m-2 col-12 col-xl-3 col-l-5 col-md-4 text-decoration-none text-dark"
     >
-      {user
-        ? Object.values(user).map((item) => {
-            return (
-              <div
-                className="card-container p-4 d-flex flex-column align-items-center"
-                key={item.id}
-              >
-                <div className="img-container">
-                  <img src={item.avatar} alt="" className="card-image " />
-                </div>
-                <div className="pt-3">
-                  <p>{item.name}</p>
-                </div>
-                <div className="pt-3">
-                  <p className="paragraph">{item.email}</p>
-                </div>
-              </div>
-            );
-          })
-        : ""}
+      <div
+        className="card-container p-4 d-flex flex-column align-items-center"
+        key={data.id}
+      >
+        <div className="img-container">
+          <img src={data.avatar} alt="" className="card-image " />
+        </div>
+        <div className="pt-3">
+          <p>{data.name}</p>
+        </div>
+        <div className="pt-3">
+          <p className="paragraph">{data.email}</p>
+        </div>
+      </div>
     </Link>
   );
 };
